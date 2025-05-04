5163bb0cb96b3ef21758bf1900b7348ad08feb57
EdJoshua11 <Ondoyladrix@gmail.com>
2025-05-04 23:36:44 +0800

    Update App.jsx
    small letter h in hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect } from "react"
-import Hero from "./components/Hero"
+import Hero from "./components/hero"
 import About from "./components/About"
 import Projects from "./components/Projects"
 import Contact from "./components/Contact"
